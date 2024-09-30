d50af38b7f5e203dce85211316f235d27513fb2d
Hakjun Kim <hakie2kim@gmail.com>
2024-09-30 13:26:14 +0900

    etc: 응답 스키마 정의 및 공통 스키마 재정의

--- api/schemas/task.py
+++ api/schemas/task.py
@@ -1,6 +1,22 @@
 from pydantic import BaseModel, Field
 
-class Task(BaseModel):
+class TaskBase(BaseModel):
+    title: str = Field(None, example="세탁소에 맡긴 것을 찾으러 가기")
+
+class TaskCreate(TaskBase):
+    pass
+
+class TaskCreateResponse(TaskCreate):
     id: int
-    title: str | None = Field(None, example="세탁소에 맡긴 것을 찾으러 가기")
+
+    class Config:
+        orm_mode = True
+
+class Task(TaskBase):
+    id: int
     done: bool = Field(False, description="완료 플래그")
+
+    class Config:
+        orm_mode = True
+
+
